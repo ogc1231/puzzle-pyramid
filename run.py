a38f1da57eef742a5ab61c055d73d03b0cc13fe1
ogc1231 <ogc@live.ie>
2023-02-08 16:02:23 +0000

    Remove uneeded comments

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-print("Welcome to level ")
+print("Level 1")
 questions = (
     "1 - What name was given to the writing system used in Ancient Egypt?",
     "2 - What was the name of the sun god worshipped in Acient Eygpt?",
