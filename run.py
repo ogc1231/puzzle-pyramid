ea86d411d1f04a6835c267f8757b1506ab99d022
ogc1231 <ogc@live.ie>
2023-02-08 10:50:06 +0000

    Add three questions and answers

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+questions = (
+    "1 - What name was given to the writing system used in Ancient Egypt?\n",
+    "2 - What was the name of the sun god worshipped in Acient Eygpt?\n",
+    "3 - Which pharaoh's tomb was discovered in 1922 by Howard Carter?\n")
+
+options = (
+    ("A - Hieroglyphics", "B - Cyrillic", "C - Graphemics"),
+    ("A - Anubis", "B - Ra", "C - Osiris"),
+    ("A - Djoser", "B - Cleopatra", "C - Tutankhamun"))
+
+print(options)
