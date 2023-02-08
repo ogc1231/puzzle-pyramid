ccb5977aa347bb64f0300add0e4b5313ef64c379
ogc1231 <ogc@live.ie>
2023-02-08 16:15:45 +0000

    Add ascii title

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-print("Level 1")
+print("""
+  _____               _        _____                           _     _ 
+ |  __ \             | |      |  __ \                         (_)   | |
+ | |__) |   _ _______| | ___  | |__) |   _ _ __ __ _ _ __ ___  _  __| |
+ |  ___/ | | |_  /_  / |/ _ \ |  ___/ | | | '__/ _` | '_ ` _ \| |/ _` |
+ | |   | |_| |/ / / /| |  __/ | |   | |_| | | | (_| | | | | | | | (_| |
+ |_|    \__,_/___/___|_|\___| |_|    \__, |_|  \__,_|_| |_| |_|_|\__,_|
+                                      __/ |                            
+                                     |___/                             
+""")
+
+# print("Level 1")
 questions = (
     "1 - What name was given to the writing system used in Ancient Egypt?",
     "2 - What was the name of the sun god worshipped in Acient Eygpt?",
