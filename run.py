e65e023a5abb4d7a3126ddd5da53f8476234e847
ogc1231 <ogc@live.ie>
2023-02-08 16:33:11 +0000

    Add while loop to start level 1, import os for cls

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
   _____               _        _____                           _     _ 
  |  __ \             | |      |  __ \                         (_)   | |
@@ -9,23 +11,27 @@ print("""
                                      |___/                             
 """)
 
-# print("Level 1")
-questions = (
-    "1 - What name was given to the writing system used in Ancient Egypt?",
-    "2 - What was the name of the sun god worshipped in Acient Eygpt?",
-    "3 - Which pharaoh's tomb was discovered in 1922 by Howard Carter?")
+while input("Ready to enter the Pyramid? Y/N ").upper() == "Y":
+    os.system("cls")
 
-options = (
-    ("A - Hieroglyphics", "B - Cyrillic", "C - Graphemics"),
-    ("A - Anubis", "B - Ra", "C - Osiris"),
-    ("A - Djoser", "B - Cleopatra", "C - Tutankhamun"))
+    print("Level 1")
 
-question_num = 0
+    questions = (
+        "1 - What name was given to the writing system used in Ancient Egypt?",
+        "2 - What was the name of the sun god worshipped in Acient Eygpt?",
+        "3 - Which pharaoh's tomb was discovered in 1922 by Howard Carter?")
 
-for question in questions:
-    print("-----------")
-    print(question)
-    for option in options[question_num]:
-        print(option)
+    options = (
+        ("A - Hieroglyphics", "B - Cyrillic", "C - Graphemics"),
+        ("A - Anubis", "B - Ra", "C - Osiris"),
+        ("A - Djoser", "B - Cleopatra", "C - Tutankhamun"))
 
-    question_num += 1
+    question_num = 0
+
+    for question in questions:
+        print("-----------")
+        print(question)
+        for option in options[question_num]:
+            print(option)
+
+        question_num += 1
