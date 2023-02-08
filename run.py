fbbcef068ed359f3114b2402430fd6d7f8f6c655
ogc1231 <ogc@live.ie>
2023-02-08 17:21:08 +0000

    comment out asscii tempariliy

--- run.py
+++ run.py
@@ -1,42 +1,42 @@
-print("""
-  _____               _        _____                           _     _ 
- |  __ \             | |      |  __ \                         (_)   | |
- | |__) |   _ _______| | ___  | |__) |   _ _ __ __ _ _ __ ___  _  __| |
- |  ___/ | | |_  /_  / |/ _ \ |  ___/ | | | '__/ _` | '_ ` _ \| |/ _` |
- | |   | |_| |/ / / /| |  __/ | |   | |_| | | | (_| | | | | | | | (_| |
- |_|    \__,_/___/___|_|\___| |_|    \__, |_|  \__,_|_| |_| |_|_|\__,_|
-                                      __/ |                            
-                                     |___/                             
-""")
+# print("""
+#   _____               _        _____                           _     _ 
+#  |  __ \             | |      |  __ \                         (_)   | |
+#  | |__) |   _ _______| | ___  | |__) |   _ _ __ __ _ _ __ ___  _  __| |
+#  |  ___/ | | |_  /_  / |/ _ \ |  ___/ | | | '__/ _` | '_ ` _ \| |/ _` |
+#  | |   | |_| |/ / / /| |  __/ | |   | |_| | | | (_| | | | | | | | (_| |
+#  |_|    \__,_/___/___|_|\___| |_|    \__, |_|  \__,_|_| |_| |_|_|\__,_|
+#                                       __/ |                            
+#                                      |___/                             
+# """)
 
-while input("Ready to enter the Pyramid? Y/N ").upper() == "Y":
-    print("Level 1")
+# while input("Ready to enter the Pyramid? Y/N ").upper() == "Y":
+#     print("Level 1")
 
-    questions = (
-        "What name was given to the writing system used in Ancient Egypt?",
-        "What was the name of the sun god worshipped in Acient Eygpt?",
-        "Which pharaoh's tomb was discovered in 1922 by Howard Carter?")
+questions = (
+    "What name was given to the writing system used in Ancient Egypt?",
+    "What was the name of the sun god worshipped in Acient Eygpt?",
+    "Which pharaoh's tomb was discovered in 1922 by Howard Carter?")
 
-    options = (
-        ("A - Hieroglyphics", "B - Cyrillic", "C - Graphemics"),
-        ("A - Anubis", "B - Ra", "C - Osiris"),
-        ("A - Djoser", "B - Cleopatra", "C - Tutankhamun"))
+options = (
+    ("A - Hieroglyphics", "B - Cyrillic", "C - Graphemics"),
+    ("A - Anubis", "B - Ra", "C - Osiris"),
+    ("A - Djoser", "B - Cleopatra", "C - Tutankhamun"))
 
-    answers = ("A", "B", "C")
-    guesses = []
-    question_num = 0
+answers = ("A", "B", "C")
+guesses = []
+question_num = 0
 
-    for question in questions:
-        print("-----------")
-        print(question)
-        for option in options[question_num]:
-            print(option)
+for question in questions:
+    print("-----------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
 
-        guess = input("Pick A, B or C ").upper()
-        guesses.append(guess)
-        if guess == answers[question_num]:
-            print("CORRECT!")
-        else:
-            print("INCORRECT!")
-            print(f"{answers[question_num]} is the correct answer")
-        question_num += 1
+    guess = input("Pick A, B or C ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        print("CORRECT!")
+    else:
+        print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
