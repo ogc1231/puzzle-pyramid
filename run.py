0e84681c29d4cdca29318f24fb857b706975fcc7
ogc1231 <ogc@live.ie>
2023-02-09 18:20:42 +0000

    Add while loop for go to shop decision

--- run.py
+++ run.py
@@ -24,11 +24,27 @@ while True:
         print("Thanks for playing, goodbye! ")
         exit()
     else:
-        print("let's go")
         break
 
 os.system('cls||clear')
-print("You finally arrive in Cario after hours of being cramped up in a small taxi with four others,")
-print("some of which looked and smelled as if they hadn't washed in weeks.")
-print("Luckily you arrive in time to gather a few items before venturing to the Pyramid tommorrow morning")
-input("")
+print("You finally arrive in Cario, after hours of being cramped up in a small")
+print("taxi with four others, some of whom looked and smelled as if they hadn't washed in weeks.\n")
+
+print("Luckily there was still some time to prep before venturing to the")
+print("Pyramid tomorrow morning, even though the sun was getting low in the sky.\n")
+
+
+while True:
+    shop = input("Go shopping for items? ")
+
+    if shop not in ["yes", "no"]:
+        print("Invalid: enter 'yes or no' ")
+        continue
+    elif shop == "no":
+        print("You find your hotel and go straight to bed ")
+        exit()
+    else:
+        print("You head to the shop to buy some items for the morning")
+        break
+
+os.system('cls||clear')
