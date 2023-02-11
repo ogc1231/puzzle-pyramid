65404b0520cde0a77a94be7fd68176e6180f118c
ogc1231 <ogc@live.ie>
2023-02-11 16:55:40 +0000

    Add player input and postion choice

--- noughts.py
+++ noughts.py
@@ -10,8 +10,35 @@ p7 = "7"
 p8 = "8"
 p9 = "9"
 
+
 print(f" {p1}  | {p2}  | {p3}")
 print(f"----+----+----")
 print(f" {p4}  | {p5}  | {p6}")
 print(f"----+----+----")
 print(f" {p7}  | {p8}  | {p9}")
+
+
+player_choice = input("Choose your position! ")
+
+if player_choice == "1":
+    p1 = "X"
+if player_choice == "2":
+    p2 = "X"
+if player_choice == "3":
+    p3 = "X"
+if player_choice == "4":
+    p4 = "X"
+if player_choice == "5":
+    p5 = "X"
+if player_choice == "6":
+    p6 = "X"
+if player_choice == "7":
+    p7 = "X"
+if player_choice == "8":
+    p8 = "X"
+if player_choice == "9":
+    p9 = "X"
+if player_choice == "1":
+    p1 = "X"
+if player_choice == "1":
+    p1 = "X"
