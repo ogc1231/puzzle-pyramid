309b5bf11addbed43a30ff50757abe18dac119b6
ogc1231 <ogc@live.ie>
2023-02-11 13:37:58 +0000

    Add player choice and mummy choice

--- rock.py
+++ rock.py
@@ -1,1 +1,10 @@
 import random
+
+hand = ("rock", "paper", "scissors")
+
+mummy_hand = random.choice(hand)
+player_hand = input("Choose a hand (rock, paper or scissors) ")
+
+
+print(f"You chose {player_hand}! ")
+print(f"The Mummy chose {mummy_hand}!")
