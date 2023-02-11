897168867dd0b302569eb9a09d395859e4c7f019
ogc1231 <ogc@live.ie>
2023-02-11 13:50:35 +0000

    Add win/lose conditions

--- rock.py
+++ rock.py
@@ -10,3 +10,14 @@ while player_hand not in hand:
 
 print(f"You chose {player_hand}! ")
 print(f"The Mummy chose {mummy_hand}!")
+
+if player_hand == mummy_hand:
+    print("It's a tie!")
+elif player_hand == "rock" and mummy_hand == "scissors":
+    print("You win!")
+elif player_hand == "scissors" and mummy_hand == "paper":
+    print("You win!")
+elif player_hand == "paper" and mummy_hand == "rock":
+    print("You win!")
+else:
+    print("You lose!")
