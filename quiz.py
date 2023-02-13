f1a358978b7b50d9cdb8d6970fdf6d9d66bfd0a4
ogc1231 <ogc@live.ie>
2023-02-13 09:12:39 +0000

    Add options for quiz questions

--- quiz.py
+++ quiz.py
@@ -1,7 +1,14 @@
-questions = (
+quiz_questions = (
     "What writing systems did the Ancient Eygptians use? ",
     "Which pharaoh's tomb was discovered in 1922 by Howard Carter? ",
-    "What name was given to the formal writing system used in Ancient Egypt ",
+    "The most famous Egyptian pyramids are found at which location? ",
     "Who was the Eygptian God of the Sun? "
     "Which ancient Egyptian charms were made in the form of beetles? ")
 
+quiz_options = (
+    ("A - Cyrillic", "B - Sanskrit", "C - Hieroglyphs"),
+    ("A - Tutankhamun", "B - Cleopatra", "C - Djoser"),
+    ("A - Alexandria", "B - Giza", "C - Nile"),
+    ("A - Osiris", "B - Isis", "C - Ra"),
+    ("A - Stag", "B - Scarabs", "C - Hercules")
+)
