e44736527ef01a8b64ffc69793be6eba2a741816
ogc1231 <ogc@live.ie>
2023-02-13 09:04:00 +0000

    Add 5 questions

---
+++ quiz.py
@@ -0,0 +1,7 @@
+questions = (
+    "What writing systems did the Ancient Eygptians use? ",
+    "Which pharaoh's tomb was discovered in 1922 by Howard Carter? ",
+    "What name was given to the formal writing system used in Ancient Egypt ",
+    "Who was the Eygptian God of the Sun? "
+    "Which ancient Egyptian charms were made in the form of beetles? ")
+
