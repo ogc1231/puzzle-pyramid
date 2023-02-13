80d6303d05c8fa2c28264001ff0f00bbdec623fb
ogc1231 <ogc@live.ie>
2023-02-13 09:29:14 +0000

    Add guess answer input and add to guess list

--- quiz.py
+++ quiz.py
@@ -25,5 +25,14 @@ for quiz_question in quiz_questions:
     for quiz_option in quiz_options[question_number]:
         print(quiz_option)
 
+    quiz_guess = input("Enter 'A', 'B' or 'C' ").upper()
+    quiz_guesses.append(quiz_guess)
+    if quiz_guess == quiz_answers[question_number]:
+        quiz_score += 1
+        print("Correct Answer!")
+    else:
+        print("Incorrect Answer!")
+    
+
     question_number += 1
 
